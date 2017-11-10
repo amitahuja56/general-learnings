d6f4cd2e565eedc75d50de967d4f7864c9166150
SAPIENT\aahu12 <aahuja13@sapient.com>
2017-11-10 22:24:56 +0530

    yo

---
+++ learnings/google-maps/main.js
@@ -0,0 +1,15 @@
+// function out() {
+//   return console.log('out');
+// }
+// var NS = NS || {};
+// (function(NS) {
+//   NS.inside = function() {
+//     return console.log('in');
+//   };
+//
+//   return NS;
+//
+// })(NS || {});
+function(){
+  // my special code
+}();
