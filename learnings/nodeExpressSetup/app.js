b7a687acdbd7220e95fad1a090352c05165c96bd
SAPIENT\aahu12 <aahuja13@sapient.com>
2017-12-11 17:02:39 +0530

    yo

--- learnings/nodeExpressSetup/app.js
+++ learnings/nodeExpressSetup/app.js
@@ -8,16 +8,17 @@ app.use(express.static('public'));
 app.use(express.static('src/views'));
 
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
     res.send('hello world');
-})
+});
 
-app.get('/books', function(req,res){
+app.get('/books', function (req, res) {
     res.send('hello books');
-})
+});
 
-app.listen(port,function(err){
-    if(err)
-    console.log(err);
+app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+    }
     console.log('express is runnnig on the port:' + port);
 });
